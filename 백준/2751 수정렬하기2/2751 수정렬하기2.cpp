2164c4237dd2ce2070224eea7388bf4e9dd25891
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 11:50:57 +0900

    수정렬하기2

---
+++ 백준/2751 수정렬하기2/2751 수정렬하기2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int n, temp;
+	scanf("%d", &n);
+	vector<int> answer;
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &temp);
+		answer.push_back(temp);
+	}
+
+	sort(answer.begin(), answer.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d\n", answer[i]);
+	}
+}
+
