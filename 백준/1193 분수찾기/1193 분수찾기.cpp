23806a38e6a7aba91dbe67907026f446e8e1d29b
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 17:33:51 +0900

    분수찾기

---
+++ 백준/1193 분수찾기/1193 분수찾기.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+	// 미해결
+
+int main() 
+{
+	int x;
+	scanf("%d", &x);
+	int i = 1;
+	while (x > i)
+	{
+		x -= i;
+		i++;
+	}
+
+	if (i % 2 == 1)
+		printf("%d/%d", i + 1 - x, x);
+	else
+		printf("%d/%d", x, i + 1 - x);
+	return 0;
+}
