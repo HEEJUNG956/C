600de5e98988e84cddb3d4143cc59b3f7d9696a5
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 14:34:58 +0900

    분산처리

---
+++ 백준/1009 분산처리/1009 분산처리.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int n;
+int a, b;
+int result = 0;
+
+void input()
+{
+    scanf("%d %d", &a, &b);
+}
+
+int solution()
+{
+    if (b % 4 == 0)
+        b = 4;
+    else
+        b %= 4;
+    if (a % 10 == 0)
+    {
+        result = 10;
+        return result;
+    }
+    else
+    {
+        a %= 10;
+        result = pow(a, b);
+        return result % 10;
+    }
+        
+}
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        input();
+        printf("%d\n", solution());
+    }
+    return 0; 
+}
+
