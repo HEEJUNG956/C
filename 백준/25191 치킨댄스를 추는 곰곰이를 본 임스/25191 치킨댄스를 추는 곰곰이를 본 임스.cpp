81852b520d755d23edc6958eefcd24d45e3e80ad
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 10:29:33 +0900

    치킨댄스

---
+++ 백준/25191 치킨댄스를 추는 곰곰이를 본 임스/25191 치킨댄스를 추는 곰곰이를 본 임스.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n; // 치킨의 개수
+    int a, b; // 콜라의 개수, 맥주의 개수
+    int answer = 0;
+    scanf("%d\n", &n);
+    scanf("%d %d", &a, &b);
+    int temp = n;
+    for (int i = 0; i < temp; i++)
+    {
+        if (a - 2 >= 0 || b - 1 >= 0 &&  n >= 0)
+        {
+            if (a - 2 >= 0)
+            {
+                a = a - 2;
+                n = n - 1;
+                answer++;
+            }
+            else
+            {
+                b = b - 1;
+                n = n - 1;
+                answer++;
+            }
+        }
+
+    }
+
+    printf("%d\n", answer);
+}
+
