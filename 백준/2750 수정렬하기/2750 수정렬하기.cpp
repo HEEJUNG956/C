533083fd9c80f25b3271aea218d7666af814ac5b
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 11:42:29 +0900

    달팽이 / 수 정렬

---
+++ 백준/2750 수정렬하기/2750 수정렬하기.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int t, temp;
+	scanf("%d", &t);
+	vector<int> answer;
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d", &temp);
+		answer.push_back(temp);
+	}
+
+	sort(answer.begin(), answer.end());
+
+	for (int i = 0; i < t; i++)
+	{
+		printf("%d\n", answer[i]);
+	}
+}
---
+++ 백준/2869 달팽이는 올라가고싶다/2869 달팽이는 올라가고싶다.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	int v, a, b; // 나무막대높이, 낮올라감, 밤내려감
+	scanf("%d %d %d", &a, &b, &v);
+	int answer;
+
+	answer = (v - b - 1) / (a - b) + 1;
+	printf("%d", answer);
+}
