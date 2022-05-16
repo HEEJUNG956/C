7e251ab8166d61e3732864d995690b05b278409f
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 12:00:50 +0900

    수정렬하기3

---
+++ 백준/10989 수정렬하기3/10989 수정렬하기3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n, temp;
+    scanf("%d", &n);
+    int count[10001] = { 0 }; // 0으로 초기화
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        count[temp]++;
+    }
+
+    for (int i = 0; i < 10001; i++)
+        for (int j = 0; j < count[i]; j++)
+            printf("%d\n", i);
+    return 0;
+}
+
