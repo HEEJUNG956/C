7b0fa73bc2b3ed807962f8994adaf4a3da3a115e
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 16:06:56 +0900

    알파벳찾기

---
+++ 백준/10809 알파벳찾기/10809 알파벳찾기.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string temp;
+    getline(cin, temp);
+    int max = 0, cnt = 0, target;
+    int a[26] = { 0 };
+    transform(temp.begin(), temp.end(), temp.begin(),(int(*)(int))toupper);
+
+    for (int i = 0; i < temp.length(); i++)
+    {
+        a[temp[i] - 'A']++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (max < a[i])
+        {
+            max = a[i];
+            cnt = 0;
+            target = i;
+        }
+        if (max == a[i])
+            cnt++;
+    }
+    if (cnt > 1)
+        printf("?");
+    else
+        printf("%c", (char)(target + 'A'));
+    return 0;
+}
