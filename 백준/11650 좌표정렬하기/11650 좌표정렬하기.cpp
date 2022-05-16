243b404b77ecf0c5e501edf19edc3caa16e7aa88
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 13:27:46 +0900

    좌표정렬하기 / 좌표정렬하기2

---
+++ 백준/11650 좌표정렬하기/11650 좌표정렬하기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    int x, y;
+    vector<pair<int, int>> answer;
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &x, &y);
+        answer.push_back({ x,y });
+    }
+    sort(answer.begin(), answer.end());
+
+    for (int i = 0; i < t; i++)
+    {
+        printf("%d %d\n", answer[i].first, answer[i].second);
+    }
+}
+
---
+++ 백준/11651 좌표정렬하기2/11651 좌표정렬하기2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int t;
+    int a, b;
+    scanf("%d", &t);
+    vector<pair<int, int>> answer;
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &a, &b);
+        answer.push_back({ b,a });
+    }
+    sort(answer.begin(), answer.end());
+
+    for (int i = 0; i < t; i++)
+    {
+        printf("%d %d\n", answer[i].second, answer[i].first);
+    }
+}
+
