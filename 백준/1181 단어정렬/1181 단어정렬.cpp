290da69f416b6d0608072614521557f957f4fcc1
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 14:02:25 +0900

    단어정렬

---
+++ 백준/1181 단어정렬/1181 단어정렬.cpp
@@ -0,0 +1,32 @@
+﻿#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool compare(string a, string b)
+{
+	if (a.length() == b.length())
+		return a < b;
+	return a.length() < b.length();
+}
+int main() 
+{
+	int n;
+	vector<string> answer;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		if (find(answer.begin(), answer.end(), str) == answer.end())
+			answer.push_back(str);
+	}
+
+	sort(answer.begin(), answer.end(), compare);
+
+	for (int i = 0; i < answer.size(); i++)
+	{
+		cout << answer[i] << "\n";
+	}
+}
