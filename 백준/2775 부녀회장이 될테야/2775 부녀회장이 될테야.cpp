0fd5e8137e89f5f15a712eb640be7b3aa19126c9
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 10:54:56 +0900

    부녀회장

---
+++ 백준/2775 부녀회장이 될테야/2775 부녀회장이 될테야.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int getNum(int a, int b)
+{
+	if (b == 1)
+		return 1;
+	if (a == 0)
+		return b;
+	return (getNum(a - 1, b) + getNum(a, b - 1));
+}
+
+
+int main()
+{
+	int t, k, n;
+	scanf("%d", &t);
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d\n%d", &k, &n);
+		printf("%d\n", getNum(k, n));
+	}
+}
