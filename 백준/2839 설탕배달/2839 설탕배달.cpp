5a107533369886bde5fd9f5e9d89ff28d4262765
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 17:19:25 +0900

    설탕배달

---
+++ 백준/2839 설탕배달/2839 설탕배달.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    // 설탕의 무게
+    int n;
+    scanf("%d", &n);
+    // 5kg개수, 3kg개수
+    int a, b;
+    a = n / 5;
+    while (1)
+    {
+        if (a < 0)
+        {
+            printf("-1");
+            return 0;
+        }
+        if ((n - (5 * a)) % 3 == 0)
+        {
+            b = (n - (5 * a)) / 3;
+            break;
+        }
+        a--;
+    }
+	printf("%d", a + b);
+}
+
