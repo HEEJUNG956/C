ae3924f7c1140b526f07f58f5609be47014c8133
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 15:13:36 +0900

    직사각형탈출

---
+++ 백준/1085 직사각형에서탈출/1085 직사각형에서탈출.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int x, y, w, h;
+int main()
+{
+    scanf("%d %d %d %d", &x, &y, &w, &h);
+    int answer1 = min(x, y);
+    int answer2 = min(w - x, h - y);
+    printf("%d", min(answer1, answer2));
+
+
+}
