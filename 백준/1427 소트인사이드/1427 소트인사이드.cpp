0f3e2d9aa121c8986ff11d784080e1df26f9c747
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 13:16:20 +0900

    소트인사이드 / 통계학(미완성)

---
+++ 백준/1427 소트인사이드/1427 소트인사이드.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string t;
+    cin >> t;
+    sort(t.begin(), t.end(), greater<char>());
+    cout << t;
+}
+
---
+++ 백준/2108 통계학/2108 통계학.cpp
@@ -0,0 +1,13 @@
+﻿#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+using namespace std;
+int n;
+int num[500001];
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
