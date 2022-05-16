128d6ba0394875818b8877d8eaa4430f4f7d0a52
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 17:44:29 +0900

    ACM호텔

---
+++ 백준/10250 ACM호텔/10250 ACM호텔.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int num, yy, xx;
+    scanf("%d", &num);
+    int* h = new int[num];
+    int* w = new int[num];
+    int* order = new int[num];
+    for (int i = 0; i < num; i++)
+        scanf("%d %d %d", &h[i], &w[i], &order[i]);
+    for (int i = 0; i < num; i++)
+    {
+        xx = order[i] / h[i] + 1;
+        yy = order[i] % h[i];
+        if (order[i] % h[i] == 0)
+        {
+            yy = h[i];
+            xx--;
+        }
+        if (xx < 10)
+            printf("%d%02d\n", yy, xx);
+        else
+            printf("%d%d\n", yy, xx);
+    }
+
+    //int t, h, w, n;
+    //scanf("%d", &t);
+    //while (t--)
+    //{
+    //    scanf("%d %d %d", &h, &w, &n);
+    //    printf("%d%02d\n", (n - 1) % h + 1, (n - 1) / h + 1);
+    //}
+}
