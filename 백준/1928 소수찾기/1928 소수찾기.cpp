2be12f2d11ede564419d35a881a1bcd3cc317a20
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-16 17:55:54 +0900

    샘Fly me to the Alpha Centauri

---
+++ 백준/1928 소수찾기/1928 소수찾기.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n, result = 0;
+    scanf("%d", &n);
+    int temp, cnt = 0;
+ 
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        for (int j = 1; j <= temp; j++)
+        {
+            if (temp % j == 0)
+                cnt++;
+        }
+        if (cnt == 2)
+            result++;
+        cnt = 0;
+    }
+
+    printf("%d", result);
+ }
