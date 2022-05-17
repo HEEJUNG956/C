f9a766892b754dfb750237a5830008a74c544bd8
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 10:07:18 +0900

    베르트랑공준

--- Sample/Sample/Sample.cpp
+++ Sample/Sample/Sample.cpp
@@ -1,21 +1,9 @@
 ﻿#include <iostream>
-
+#include <vector>
+#include <cmath>
+using namespace std;
 int main()
 {
-    int n, temp;
-    scanf("%d", &n);
-    int cnt = 0, count = 0;
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &temp);
-        for (int j = 1; j <= temp; j++)
-        {
-            if (temp % j == 0)
-                cnt++;
-        }
-        if (cnt == 2)
-            count++;
-        cnt = 0;
-    }
-    printf("%d", count);
+	
 }
+
