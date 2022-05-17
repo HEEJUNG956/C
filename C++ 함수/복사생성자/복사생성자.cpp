05fc9569edb8fbed23c2b305a7ea22893f3c778a
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 13:38:25 +0900

    소멸자 / 복사생성자

---
+++ C++ 함수/복사생성자/복사생성자.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+
+// 복사생성자 = 클래스이름(const 클래스이름 &참조변수)
+// 상수로 선언된 참조자이며 type은 자기자신이 되는 것
+// 기존값을 복사해 전달해주는 개념
+// 대입연산 할때, 매개변수로 들어갈때, 반환값이 있을때 호출
+
+class A
+{
+    int num;
+    string name;
+public:
+    A(){}
+    A(int num, string name)
+    {
+        this -> num = 10;
+        this -> name = name;
+    }
+
+    A(const A& ref)
+    {
+        num = ref.num;
+        name = ref.name;
+        printf("복사생성자호출\n");
+    }
+
+    void fct_1(A obj)
+    {
+        printf("fct_1 함수실행\n");
+    }
+
+    A fct_2(void)
+    {
+        A obj;
+        printf("fct_2 함수실행\n");
+        return obj;
+    }
+};
+int main()
+{
+    A a(10, "jung");
+    a.fct_1(a);
+    printf("fct_1 함수 실행 끝\n");
+    A b = a.fct_2();
+    printf("fct_2 함수 실행 끝\n");
+}
---
+++ C++ 함수/소멸자/소멸자.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+// 소멸자란 객체의 사용이 끝나고 메모리가 반환될때 실행되는 코드
+// ~ 로 시작
+class A
+{
+    int* num;
+public:
+    A()
+    {
+        num = new int[10];
+    }
+
+    ~A()
+    {
+        delete num;
+    }
+};
+int main()
+{
+    A a;
+    return 0;
+}
