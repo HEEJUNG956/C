0519123c99a40ea08a1c037406595bf103bfa89e
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 11:29:49 +0900

    함수

---
+++ C++ 함수/객체,인스턴스,클래스/객체,인스턴스,클래스.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+class Creaker
+{
+private:
+    int flour;
+    int salt;
+    int sugar;
+    int water;
+    int rainbowFlour;
+    int total_weight;
+    const char* creaker_name;
+
+public:
+    Creaker(int flour, int salt, int sugar, int water,
+        int rainbowFlour, const char * creaker_name)
+    {
+        this->flour = flour;
+        this->salt = salt;
+        this->water = water;
+        this->rainbowFlour = rainbowFlour;
+        this->creaker_name = creaker_name;
+    }
+
+    void mixer(void)
+    {
+        total_weight = flour + salt + sugar + water + rainbowFlour;
+    }
+
+    void print_Creaker(void)
+    {
+        printf("완성 ! 총 용량 = %d\n", total_weight);
+        printf("과자 이름 : %c\n", creaker_name);
+    }
+};
+
+int main(void)
+{                   // 객체 부분( 인스턴스 부분 )
+    Creaker* Dieje = new Creaker(100, 20, 10, 100, 10, "Dideje");
+    Creaker* pokacip = new Creaker(100, 20, 10, 100, 10, "pokacip");
+    Creaker* Homenumboll = new Creaker(100, 20, 10, 100, 10, "Homenumboll");
+
+    return 0;
+}
---
+++ C++ 함수/구조체와 클래스/구조체와 클래스.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+struct MyStruct
+{
+    int num1;
+    char arr1[10];
+    
+    void print(void)
+    {
+        printf("My Struct\n");
+        printf("num = %d\n", num1);
+        printf("arr[0] = %c\n", arr1);
+    }
+};
+
+class MyClass
+{
+public:
+    int num2;
+    char arr2[10];
+
+    void print(void)
+    {
+        printf("My Struct\n");
+        printf("num = %d\n", num2);
+        printf("arr[0] = %c\n", arr2);
+    }
+};
+int main()
+{
+    MyStruct m = { 10, "MY" };
+    m.print();
+
+    MyClass c = { 10, "MY" };
+    c.print();
+
+    return 0;
+}
+
