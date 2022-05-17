f14107a859d4d09f2553b152c117d4b9b6ca308e
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 14:58:39 +0900

    템플릿 / 템플릿클래스

---
+++ C++ 함수/템플릿/템플릿.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+// 여러 객체 사용시 템플릿 사용
+template <class T>
+T Adder(T x, T y)
+{
+    return x + y;
+}
+
+template<>
+const char* Adder(const char* arr1, const char* arr2)
+{
+    char* a = new char[100];
+    strcpy(a, arr1);
+    strcat(a, arr2);
+    return a;
+}
+int main()
+{
+    printf("%s\n", Adder("hello ", "world"));
+    printf("%d\n", Adder(10, 20));
+    printf("%d\n", Adder(1.5, 20.999));
+}
---
+++ C++ 함수/템플릿클래스/템플릿클래스.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+class Calculator
+{
+private:
+    T x, y;
+public :
+    Calculator(T n1, T n2) : x(n1), y(n2){}
+    T Adder();
+};
+
+template<class T>
+T Calculator<T>::Adder()
+{
+    return x + y;
+}
+
+int main()
+{
+    Calculator<int> cal1(10, 20);
+    Calculator<double> cal2(1.5, 20.9999);
+    
+    printf("%d\n", cal1.Adder());
+    printf("%f\n", cal2.Adder());
+    return 0;
+}
