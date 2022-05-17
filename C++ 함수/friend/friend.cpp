317c71b9a16b05c865f25baa035f928786388921
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 14:14:31 +0900

    friend / virtual

---
+++ C++ 함수/friend/friend.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class A
+{
+private:
+    int num;
+    string name;
+    friend class B;
+public:
+    A(){}
+
+    A(int num, string name)
+    {
+        this->num = num;
+        this->name = name;
+    }
+};
+
+class B 
+{
+    A* a;
+public:
+    void createA()
+    {
+        a = new A(10, "haha");
+    }
+
+    int sum()
+    {
+        return a->num + 10;
+    }
+
+    void fix_Name(string name)
+    {
+        a->name = name;
+    }
+
+    void showA()
+    {
+        cout << a->name << endl;
+    }
+};
+
+// Class B가 A의 private변수에 접근해서 그 변수를 사용하고있음
+// 정보은닉 위반방법으로 좋은 방법은 아님
+int main()
+{
+    B b;
+    b.createA();
+    b.sum();
+    b.fix_Name("choi");
+    b.showA();
+
+    return 0;
+}
---
+++ C++ 함수/virtual/virtual.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+// virtual(가상함수)
+// 자식클래스가 부모클래스의 함수를 가리는 형태가되어
+// 자식클래스의 함수가 호출되게하는 것이 오버라이딩
+// 즉 함수의 재정의
+
+class A
+{
+private:
+	int num = 100;
+public:
+	virtual void show_P()
+	{
+		printf("class A : %d\n", this->num);
+	}
+
+	void show_T()
+	{
+		printf("class A : %d\n", this->num);
+	}
+
+};
+
+class B : public A
+{
+private:
+	int num = 999999;
+public:
+	virtual void show_P()
+	{
+		printf("class B : %d\n", this->num);
+	}
+	void show_T()
+	{
+		printf("class B : %d\n", this->num);
+	}
+};
+
+int main()
+{
+	A* a_1 = new A();
+	A* a_2 = new B();
+
+	a_1->show_P();
+	a_1->show_T();
+
+	printf("\n");
+	printf("\n");
+
+	// 가상함수로 선언된 멤버함수가 있고
+	// 이를 오버라이딩한 자식클래스가 존재한다면
+	// 형변환이 이루어진 포인터 변수는 
+	// 자식클래스의 오버라이딩된 함수를 호출 할수 있는 기능생성
+	a_2->show_P();
+	a_2->show_T();
+	return 0;
+}
