f37d08d5ed1712d54a38c1b877bc046ba84ffb6c
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 11:51:02 +0900

    생성자

---
+++ C++ 함수/생성자/생성자.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// 생성자 : 객체 선언시 자동으로 실행되는 코드(초기값 부여시 사용)
+
+class A {
+private:
+    int num;
+    string name;
+public:
+    A(int num, char* name)
+    {
+        this->num = num;
+        this->name, name;
+    }
+};
+int main()
+{
+    A a(10, "Jung");
+
+    return 0;
+}
+
