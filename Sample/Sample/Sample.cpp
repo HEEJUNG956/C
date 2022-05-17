9adedf540ca5d515720a8511f5a33cb158b503d3
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 09:21:34 +0900

    소수

--- Sample/Sample/Sample.cpp
+++ Sample/Sample/Sample.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int n, temp;
+    scanf("%d", &n);
+    int cnt = 0, count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &temp);
+        for (int j = 1; j <= temp; j++)
+        {
+            if (temp % j == 0)
+                cnt++;
+        }
+        if (cnt == 2)
+            count++;
+        cnt = 0;
+    }
+    printf("%d", count);
 }
---
+++ 백준/2581 소수/2581 소수.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+    int m, n;
+    int max = 0, min = -1, cnt = 0;
+    int count = 0;
+    scanf("%d\n%d", &m, &n);
+    for (int i = m; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if (i % j == 0)
+                cnt++;
+        }
+        if (cnt == 2)
+        {
+            if (min == -1)
+            {
+                min = i;
+            } 
+            max += i;
+            count++;
+        }
+        cnt = 0;
+    }
+
+    if (count == 0)
+    {
+        printf("-1");
+        return 0;
+    }
+    printf("%d\n%d", max, min);
+    return 0;
+}
+
