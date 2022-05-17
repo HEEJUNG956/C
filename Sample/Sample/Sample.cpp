696684775f9dca4cef3aaa3132bb4060201074f6
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 11:02:28 +0900

    골드바흐의추측

--- Sample/Sample/Sample.cpp
+++ Sample/Sample/Sample.cpp
@@ -1,9 +1,18 @@
 ﻿#include <iostream>
-#include <vector>
-#include <cmath>
-using namespace std;
+
+
 int main()
 {
-	
-}
+	int num = 10;
+	int* p = &num;
 
+	int sum = num + *p;
+	printf("변수의 주소값 p : %d\n", p);
+	printf("변수의 값 *p : %d\n", *p);
+	printf("주소 값 &p : %d\n", &p);
+
+	//p = p가 가르키고 있는 변수의 주소 값(num의 주소 값)
+	//* p = p가 가르키고 있는 변수의 값(num의 값)
+	//& p = p의 주소 값
+	return 0;
+}
---
+++ 백준/9020 골드바흐의추측/9020 골드바흐의추측.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int max = 0;
+	int t;
+	scanf("%d", &t);
+	int* even = new int[t];
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d", &even[i]);
+		if (even[i] > max)
+			max = even[i];
+	}
+
+	bool* prime = new bool[max + 1];
+	fill_n(prime, max + 1, 1);
+	prime[0] = false, prime[1] = false;
+	for (int i = 2; i <= sqrt(max); i++)
+		if (prime[i] == true)
+			for (int j = i * 2; j <= max; j += i)
+				prime[j] = false;
+
+	for (int t1 = 0; t1 < t; t1++)
+	{
+		for (int j = even[t1] / 2; j > 0; j--)
+		{
+			if (prime[j] && prime[even[t1] - j])
+			{
+				printf("%d %d\n", j, even[t1] - j);
+			}
+		}
+	}
+}
+
+
+// 8 2 3 5 7
