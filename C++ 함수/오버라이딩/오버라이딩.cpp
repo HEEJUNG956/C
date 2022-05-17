489aed0032990195fad94d10deaa559eb90553ba
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-17 13:50:16 +0900

    오버로딩, 오버라이딩

---
+++ C++ 함수/오버라이딩/오버라이딩.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+// 오버로딩과 다르게 함수의 반환값, 매개변수, 이름 모두 동일
+// virtual기능 사용시 많은 도움
+
+// A의 자식클래스인 B는 class A의 모든 함수와 멤버 변수를 사용가능
+// B에서 A의 함수가 맘에 안들면 새로 함수를 같은이름으로 
+// 정의하는 것을 오버라이딩이라고 부릅니다.
+class A 
+{
+    int num = 10;
+public :
+    void show_Pt() 
+    {
+        printf("class A의 num : %d\n", num);
+    }
+};
+
+class B : public A
+{
+    int num = 999;
+public:
+    void show_Pt()
+    {
+        printf("class B의 num : %d\n", num);
+    }
+};
+int main()
+{
+    A a;
+    B b;
+    a.show_Pt();
+    b.show_Pt();
+}
---
+++ C++ 함수/오버로딩/오버로딩.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+// 함수의 오버로딩을 할 시 반환값의 자료형은 동일하게하고
+// 매기변수는 달라야 한다.
+void fct_1(void)
+{
+    printf("나는 fuc_1()!!\n");
+}
+void fct_1(int num)
+{
+    printf("나는 fct_1(int num!!) : %d\n", num);
+}
+int fct_2(int num2)
+{
+    printf("나는 fct_2 int 형이다 : %d\n", num2);
+    return num2;
+}
+
+int main()
+{
+    fct_1();
+    fct_1(111);
+
+    int num;
+    num = fct_2(777);
+    printf("num의 값 : %d\n", num);
+    return 0;
+}
