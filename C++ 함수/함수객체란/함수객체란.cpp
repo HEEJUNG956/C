e1be001591d5e8421879e1528369cf6cd67fb30e
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-18 09:10:29 +0900

    수정

--- C++ 함수/함수객체란/함수객체란.cpp
+++ C++ 함수/함수객체란/함수객체란.cpp
@@ -1,44 +1,63 @@
 ﻿#include <iostream>
 using namespace std;
-
-class Plus {
+class Plus
+{
 public:
-    Plus() {
-        num = 0;
-    }
-
-    int operator()() {
-        num++;
-        return num;
-    }
-
-    int operator()(int n) {
-        num = ++n;
-        return num;
-    }
-
-    friend ostream& operator<<(ostream&, const Plus&);
+	Plus()
+	{
+		num = 0;
+	}
+	int operator()()
+	{
+		num++;
+		return num;
+	}
+	friend ostream& operator<<(ostream&, const Plus&);
 
 private:
-    int num;
+	int num;
+};
+class Minus
+{
+public:
+	Minus()
+	{
+		num = 0;
+	}
+	int operator()()
+	{
+		num--;
+		return num;
+	}
+	friend ostream& operator<<(ostream&, const Minus&);
 
+private:
+	int num;
 };
 
-ostream& operator<<(ostream& os, const Plus& plus) {
-    os << plus.num << endl;
-    return os;
+ostream& operator<<(ostream& os, const Plus& plus)
+{
+	os << plus.num << endl;
+	return os;
 }
 
-int main() {
-    Plus plus;
-    int num;
-    num = plus(5);
+ostream& operator<<(ostream& os, const Minus& minus)
+{
+	os << minus.num << endl;
+	return os;
+}
 
-    cout << num << endl;
+template<class T>
+T choice(T op)
+{
+	return op;
+}
 
-    for (int i = 0; i < 5; i++)
-        plus();
-
-    cout << plus << endl;
-    return 0;
+int main()
+{
+	Plus plus;
+	Minus minus;
+	printf("%d\n", choice(plus()));
+	printf("%d\n", choice(minus()));
+	return 0;
 }
