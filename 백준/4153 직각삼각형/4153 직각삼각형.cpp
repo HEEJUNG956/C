f575889abc42c92eff5681cb4dc012f81c4ed759
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-18 16:01:17 +0900

    직각삼각형

---
+++ 백준/4153 직각삼각형/4153 직각삼각형.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    while (1)
+    {
+        scanf("%d %d %d", &a, &b, &c);
+        if (a == 0)
+            break;
+        if(max(a,b) == a)
+            if (max(a, c) == a)
+            {
+                if (a * a == b * b + c * c) printf("right\n");
+                else printf("wrong\n");
+            }
+            else
+            {
+                if (c * c == a * a + b * b) printf("right\n");
+                else printf("wrong\n");
+            }
+        else
+            if (max(b, c) == b)
+            {
+                if (b * b == a * a + c * c) printf("right\n");
+                else printf("wrong\n");
+            }
+            else
+            {
+                if (c * c == a * a + b * b) printf("right\n");
+                else printf("wrong\n");
+            }
+    }
+}
