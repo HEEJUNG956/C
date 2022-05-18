0ac211680dafa155a8292d5229243b30fdf3bf6b
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-18 14:31:00 +0900

    파일입출력

---
+++ C++ 함수/파일입출력 Window전용/파일입출력 Window전용.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <shlobj_core.h>
+
+using namespace std;
+string text = "성자신손 오백 년은 우리 황실이요\
+산수 고려 동반도는 우리 본국일세\
+무궁화 삼천리 화려강산\
+조선 사람 조선으로 길이 보존하세\
+이천만 오직 한 마음 나라 사랑하여\
+사농공상 상하 없이 제 직분 다하세\
+무궁화 삼천리 화려강산\
+조선 사람 조선으로 길이 보존하세";
+
+
+void create_filename(PWSTR path, WCHAR* cp_path, string s)
+{
+    int i = 0;
+    while (path[i] != '\0') cp_path[i] = path[i++];
+    for_each(s.begin(), s.end(), [&](char& c) {cp_path[i++] = c;});
+    cp_path[i] = '\0';
+}
+int main()
+{
+    PWSTR path = NULL; WCHAR cp_path[100];
+    SHGetKnownFolderPath(FOLDERID_Desktop, 0, NULL, &path);
+    create_filename(path, cp_path, "\\text.txt");
+
+    ofstream writeFile(cp_path);
+    if (writeFile.is_open())
+    {
+        writeFile << text;
+        writeFile.close();
+    }
+
+    ifstream readFile(cp_path);
+
+    if (readFile.is_open())
+    {
+        string line;
+        while (getline(readFile, line))
+        {
+            printf("%s\n", line.c_str());
+        }
+        readFile.close();
+    }
+
+    CoTaskMemFree(path);
+    return 0;
+}
+
---
+++ C++ 함수/파일입출력 사용자Window경로가져오기/파일입출력 사용자Window경로가져오기.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <ShlObj_core.h>
+using namespace std;
+int main()
+{
+    PWSTR path = NULL;
+    SHGetKnownFolderPath(FOLDERID_Desktop, 0, NULL, &path);
+
+    wprintf(L"%s\n", path);
+    CoTaskMemFree(path);
+
+    SHGetKnownFolderPath(FOLDERID_Documents, 0, NULL, &path);
+    wprintf(L"%s\n", path);
+    CoTaskMemFree(path);
+    return 0;
+}
+
