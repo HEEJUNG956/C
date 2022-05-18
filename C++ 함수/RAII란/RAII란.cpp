ec4fca9fe0cc3cbb15a7c1bd7af4011fa4a1c073
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-18 15:37:42 +0900

    RAII란

---
+++ C++ 함수/RAII란/RAII란.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <mutex>
+using namespace std;
+
+//RAII Resource acquisition is initialization
+// 자원 획득을 초기화한다.
+bool error()
+{
+    return true;
+}
+
+void fnc()
+{
+    int* c = new int[100];
+    unique_ptr<int> q(c);
+    if (error()) return;
+    delete[] c;
+
+    mutex m;
+    m.lock();
+}
+int main()
+{
+    fnc();
+    printf("hi");
+    _CrtDumpMemoryLeaks();
+    return 0;
+}
