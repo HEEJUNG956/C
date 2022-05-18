c5679017380bea0e52a7aca9a6aef44fcbcdfcf1
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-18 09:27:51 +0900

    함수포인터

---
+++ C++ 함수/함수포인터/함수포인터.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+typedef bool(*Cmp)(int, int);
+bool Up(int x, int y) { return (x > y ? true : false); }
+bool Down(int x, int y) { return (x < y ? true : false); }
+
+void simple_sort(int* arr, int n, Cmp cmp)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		for (int j = i + 1; j < 5; j++)
+		{
+			if (cmp(arr[i], arr[j]))
+			{
+				arr[i] ^= arr[j] ^= arr[i] ^= arr[j];
+			}
+		}
+	}
+}
+
+void sort_print(int* arr, int n)
+{
+	for (int i = 0; i < 5; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+int main(void)
+{
+	int arr[5] = { 10, 5, 41, 100, 2 };
+
+	simple_sort(arr, 5, Down);
+	sort_print(arr, 5);
+
+	simple_sort(arr, 5, Up);
+	sort_print(arr, 5);
+
+	return 0;
+}
