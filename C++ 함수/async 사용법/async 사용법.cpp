c7a32d4848d0c9662797f61234f4caaeadf0ad2d
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-18 14:59:20 +0900

    thread / async

---
+++ C++ 함수/async 사용법/async 사용법.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <future>
+using namespace std;
+void for_print(char c)
+{
+    for (int i = 0; i < 10; i++)
+        printf("%c번 Thread : %d\n", c, i);
+}
+int main()
+{
+    auto c = async(for_print, 'c');
+    future<int> d;
+
+    try
+    {
+        c.get();
+        d.get();
+    }
+    catch (const future_error& e)
+    {
+        printf("\nCaught a future_error with code %d\n", e.code());
+        printf("Message : %s\n", e.what());
+    }
+
+    return 0;
+}
---
+++ C++ 함수/thread와 async차이점/thread와 async차이점.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <future>
+#include <cstdio>
+using namespace std;
+void for_print(int num)
+{
+    for (int i = 0; i < 100; i++)
+        printf("%d번 Thread : %d\n", num, i);
+}
+int main()
+{
+    // thread 계산
+    //vector<thread> v_thread;
+    //v_thread.reserve(2000);
+
+    //for (size_t i = 0; i < 2000; i++)
+    //    v_thread.emplace_back(std::thread(for_print, i));
+
+    //for (size_t i = 0; i < 2000; i++)
+    //    v_thread[i].join();
+
+    //vector<thread>().swap(v_thread);
+    //return 0;
+
+
+    // async 계산
+    vector<future<void>> v_async;
+    v_async.reserve(2000);
+
+    for (size_t i = 0; i < 2000; i++)
+        v_async.emplace_back(async(launch::async, for_print, i));
+
+    for (size_t i = 0; i < 2000; i++)
+        v_async[i].wait();
+
+    return 0;
+}
