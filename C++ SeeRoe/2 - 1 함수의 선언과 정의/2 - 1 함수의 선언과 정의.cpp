92cb4dc23cf9abba6dd0ef6ca2fa3dbb53cc92a2
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-19 15:10:09 +0900

    중간

---
+++ C++ SeeRoe/2 - 1 함수의 선언과 정의/2 - 1 함수의 선언과 정의.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int Plus(int x, int y);
+float Plus(float x, float y);
+char Plus(char x, char y);
+double Plus(double x, double y);
+int main()
+{
+    cout << Plus(3.5, 5.5) << endl;
+    cout << Plus(3, 5) << endl;
+    cout << Plus((float)3.5, (float)5.5) << endl;
+    cout << Plus((char)65, (char)32) << endl;
+    //return 0;
+}
+
+
+int Plus(int x, int y)
+{
+    return x + y;
+}
+float Plus(float x, float y)
+{
+    return x + y;
+}
+char Plus(char x, char y)
+{
+    return x + y;
+}
+double Plus(double x, double y)
+{
+    return x + y;
+}
