f879236614401a0d5a00cf300fee1da8c8557e21
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-19 13:43:04 +0900

    1

--- Sample/Sample/Sample.cpp
+++ Sample/Sample/Sample.cpp
@@ -1,18 +1,25 @@
-﻿#include <iostream>
-
-
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
 int main()
 {
-	int num = 10;
-	int* p = &num;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    char temp;
 
-	int sum = num + *p;
-	printf("변수의 주소값 p : %d\n", p);
-	printf("변수의 값 *p : %d\n", *p);
-	printf("주소 값 &p : %d\n", &p);
+    if (a && b == 1)
+        printf("참\n");
+    else
+        printf("거짓\n");
 
-	//p = p가 가르키고 있는 변수의 주소 값(num의 주소 값)
-	//* p = p가 가르키고 있는 변수의 값(num의 값)
-	//& p = p의 주소 값
-	return 0;
+    if (a || b == 1)
+        printf("참\n");
+    else
+        printf("거짓\n");
+
+    if (!a == 1)
+        printf("참\n");
+    else
+        printf("거짓\n");
+
+    return 0;
 }
