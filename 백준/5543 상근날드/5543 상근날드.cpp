78c7294e7f1c213485f49d070716254c837c6b11
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-19 13:11:40 +0900

    상근날드 / 덩치

---
+++ 백준/5543 상근날드/5543 상근날드.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int burgers[3];
+    int drinks[2];
+    int cheapest_burger = 9999;
+    int cheapest_beverage = 9999;
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%d", burgers[i]);
+        if (burgers[i] < cheapest_burger)
+            cheapest_burger = burgers[i];
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        cin >> drinks[i];
+        if (drinks[i] < cheapest_beverage)
+            cheapest_beverage = drinks[i];
+    }
+    printf("%d", cheapest_beverage + cheapest_burger - 50);
+
+}
---
+++ 백준/7568 덩치/7568 덩치.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <utility>
+using namespace std;
+int main()
+{
+    int t;
+    int rank = 1;
+    pair<int, int> arr[50];
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d", &arr[i].first, &arr[i].second);
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        for (int j = 0; j < t; j++)
+            if (arr[i].first < arr[j].first && arr[i].second < arr[j].second)
+                rank++;
+        printf("%d ", rank);
+        rank = 1;
+    }
+}
