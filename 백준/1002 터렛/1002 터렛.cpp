73cc6bfb0e15868ecb60739f9bcab78302fe2506
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-19 09:13:00 +0900

    터렛 / 팩토리얼

---
+++ 백준/1002 터렛/1002 터렛.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int x1, y1, r1, x2, y2, r2, d, cond1, cond2, t;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d %d %d %d %d %d", &x1, &y1, &r1, &x2, &y2, &r2);
+        d = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+        cond1 = (r1 - r2) * (r1 - r2);
+        cond2 = (r1 + r2) * (r1 + r2);
+
+        if (d == 0)
+        {
+            if (cond1 == 0)
+                printf("-1\n");
+            else
+                printf("0\n");
+        }
+        else if (d == cond1 || d == cond2)
+            printf("1\n");
+        else if (cond1 < d && d < cond2)
+            printf("2\n");
+        else
+            printf("0\n");
+    }
+}
---
+++ 백준/10872 팩토리얼/10872 팩토리얼.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int t;
+    int answer = 1;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        answer *= i;
+    }
+    printf("%d", answer);
+}
+
