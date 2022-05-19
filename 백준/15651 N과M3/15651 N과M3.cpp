20eec71882d8f397bbf07afc27c406128ea3381a
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-19 14:22:43 +0900

    N과M

---
+++ 백준/15651 N과M3/15651 N과M3.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[9] = { 0 };
+bool visited[9] = { 0 };
+
+void dfs(int cnt)
+{
+    if (cnt == m)
+    {
+        for (int i = 0; i < m; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+        return;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        visited[i] = true;
+        arr[cnt] = i;
+        dfs(cnt + 1);
+        visited[i] = false;
+    }
+}
+int main()
+{
+    scanf("%d %d", &n, &m);
+    dfs(0);
+}
