a88a424d7f76e0777b2f3689ef1de58e7cf3528d
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-20 12:03:56 +0900

    업앤다운 완성

--- Sample/계산기/계산기.cpp
+++ Sample/계산기/계산기.cpp
@@ -1,23 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
 
-int temp;
+int temp, a, b;
+char c;
 void menu();
 
 int main()
 {
-    int q;
-    printf("값을 입력해주세요 : ");
-    cin >> q;
+    for (;;)
+    {
+        printf("계산기 시작 : ");
+        cin >> c;
+        
+        if (c == 'y' || c == 'Y')
+        {
+            printf("값을 입력해주세요 : ");
+            cin >> a;
 
-    printf("1. 더하기\n");
-    printf("2. 빼기\n");
-    printf("3. 곱하기\n");
-    printf("4. 나누기\n");
-    printf("원하시는 수식을 입력해주세요 : ");
-    cin >> temp;
+            printf("1. 더하기\n");
+            printf("2. 빼기\n");
+            printf("3. 곱하기\n");
+            printf("4. 나누기\n");
+            printf("원하시는 수식을 입력해주세요 : ");
+            cin >> temp;
 
-    menu();
+            menu();
+        }
+        else if (c == 'n' || c == 'N')
+        {
+            printf("종료합니다. \n");
+            break;
+        }
+        else
+        {
+            printf("잘못입력하셨습니다. \n");
+        }
+            
+       }
+
 }
 
 void menu()
@@ -25,15 +45,26 @@ void menu()
     switch (temp)
     {
     case 1:
+        printf("더할 값을 입력해주세요 : ");
+        cin >> b;
+        printf("%d + %d = %d\n", a, b, a + b);
         break;
     case 2:
+        printf("뺄 값을 입력해주세요 : ");
+        cin >> b;
+        printf("%d - %d = %d\n", a, b, a - b);
         break;
     case 3:
+        printf("곱할 값을 입력해주세요 : ");
+        cin >> b;
+        printf("%d * %d = %d\n", a, b, a * b);
         break;
     case 4:
+        printf("나눌 값을 입력해주세요 : ");
+        cin >> b;
+        printf("%d / %d = %d\n", a, b, a / b);
         break;
     default:
-        printf("존재하지 않는 수식입니다.");
-        break;
+        printf("잘못 입력하셨습니다. \n");
     }
 }
---
+++ Sample/업앤다운/업앤다운.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <random>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    int min = 0, max = 100;
+    int answer = (rand() % 100);
+    int count = 0;
+    int temp;
+
+    for(;;)
+    {
+        printf("%d\n", answer);
+        printf("과제명 : 업앤다운\n");
+        printf("MIN : %d MAX : %d\n", min, max);
+        printf("시도 횟수 : %d\n", count);
+        printf("범위내 숫자를 입력하세요.\n");
+        cin >> temp;
+        if (answer > temp)
+        {
+            if (temp < min)
+            {
+                printf("min 값보다 작은 값입니다. ");
+                printf("다시 입력해주세요\n");
+                system("PAUSE");
+            }
+            else
+            {
+                printf("업\n");
+                min = temp;
+                count++;
+                system("PAUSE");
+            }
+        }
+        else if (answer < temp)
+        {
+            if (temp > max)
+            {
+                printf("max 값보다 큰 값입니다. ");
+                printf("다시 입력해주세요\n");
+                system("PAUSE");
+            }
+            else
+            {
+                printf("다운\n");
+                max = temp;
+                count++;
+                system("PAUSE");
+            }
+        }
+        else if (temp == answer)
+        {
+            printf("정답입니다!\n");
+            printf("시도 횟수 : %d\n", count);
+            break;
+        }
+        system("cls");
+    }
+}
+
