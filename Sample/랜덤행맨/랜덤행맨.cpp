29472ebcd17d02ade0f3882c72ff4b6c51aca7c9
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-23 11:46:58 +0900

    문자맞추기/ 로켓발사

--- Sample/랜덤행맨/랜덤행맨.cpp
+++ Sample/랜덤행맨/랜덤행맨.cpp
@@ -8,6 +8,64 @@ using namespace std;
 
 int main() 
 {  
-    return 0;
+    srand((unsigned int)time(0));
+
+    char word[5][7] = { "banana", "rocket", "yellow", "orange", "secret" };
+
+    char* answer = word[rand() % 5];
+    
+    printf("%c\n", &answer);
+
+    //char answer[5];
+    char blind[] = "______";
+
+
+    char c;
+    int cnt, key, life = 10;
+
+    while (1)
+    {
+        cnt = 0;
+        key = 0;
+        printf("%s : ", blind);
+        scanf_s(" %c", &c);
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (c == blind[i])
+            {
+                key = 1;
+                printf("이미 등록된 문자입니다 재입력해주세요. \n");
+                break;
+            }
+            else if (c == answer[i])
+            {
+                blind[i] = c;
+                cnt++;
+            }
+        }
+
+        if (key == 1)
+        {
+            continue;
+        }
+
+        if (cnt == 0)
+        {
+            life--;
+            if (life == 0)
+            {
+                printf("게임종료...\n");
+                break;
+            }
+        }
+
+        if (strcmp(blind, answer) == 0)
+        {
+            printf("%s\n", blind);
+            printf("정답!!!");
+            break;
+        }
+    }
 }
 
--- Sample/로켓 발사/로켓 발사.cpp
+++ Sample/로켓 발사/로켓 발사.cpp
@@ -1,15 +1,22 @@
 ﻿#include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    printf("과제명 : 로켓\n");
-    printf("--하늘----하늘-----하늘-----구름----구름\n");
-    for(;;)
+    int temp = 15;
+    for (;;)
     {
-        printf("  ▲ \n");
-        printf("◁▤▷\n");
-        printf(" ┘└ \n");
-        printf("wwwww\n");
+        printf("과제명 : 로켓\n");
+        printf("--하늘----하늘-----하늘-----구름----구름\n");
+        {
+            for (int i = temp; i >= 0; i--)
+                printf("%d\n", i);
+            printf("  ▲ \n");
+            printf("◁▤▷\n");
+            printf(" ┘└ \n");
+            printf("wwwww\n");
+        }
+     system("cls");
     }
 
     
