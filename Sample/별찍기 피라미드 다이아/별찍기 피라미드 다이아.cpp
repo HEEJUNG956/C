585b7a01e12326f10eb8357d69bf38cf0d71b5e7
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-23 14:26:51 +0900

    별찍기

---
+++ Sample/별찍기 피라미드 다이아/별찍기 피라미드 다이아.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int temp;
+    while (1)
+    {
+        printf("별찍기 연습 (피라미드, 다이아)\n");
+        printf("찍을 개수 : ");
+        scanf_s("%d", &temp);
+
+        printf("직각삼각형\n");
+        for (int i = 0; i < temp; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                printf("*");
+            printf("\n");
+        }
+        printf("\n");
+
+        printf("피라미드\n");
+        for (int i = 1; i <= temp; i++)
+        {
+            for (int j = temp; j > i; j--)
+                printf(" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                printf("*");
+            printf("\n");
+        }
+        printf("\n");
+
+        printf("다이아몬드\n");
+        for (int i = 1; i <= temp; i++)
+        {
+            for (int j = temp; j > i; j--)
+                printf(" ");
+            for (int j = 1; j <= 2 * i - 1; j++)
+                printf("*");
+            printf("\n");
+        }
+        for (int i = 1; i <= temp; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                printf(" ");
+            }
+            for (int j = 2 * temp - 1; j > 2 * i; j--)
+            {
+                printf("*");
+            }
+            printf("\n");
+        }
+    }
+    
+}
+
