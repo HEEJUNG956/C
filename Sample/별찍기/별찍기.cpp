ac4c12f45ac10c7bec530e31f47ac0eb3961f9d5
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-23 10:22:20 +0900

    별찍기

---
+++ Sample/별찍기/별찍기.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int temp;
+    printf("별찍기 문제\n");
+    printf("라인수를 입력하세요 : ");
+    scanf_s("%d", &temp);
+
+    printf("정사각형\n");
+    for (int i = 0; i < temp; i++)
+    {
+        for (int j = 0; j < temp; j++)
+            printf("*");
+        printf("\n");
+    }
+
+    printf("\n직각삼각형\n");
+    for (int i = 0; i < temp; i++)
+    {
+        for (int j = 0; j <= i; j++)
+            printf("*");
+        printf("\n");
+    }
+
+
+    printf("\n역직각삼각형\n");
+    for (int i = 0; i < temp; i++)
+    {
+        for (int j = 0; j < i; j++)
+            printf(" ");
+        for (int j = temp; j > i; j--)
+            printf("*");
+        printf("\n");
+    }
+
+
+    printf("\n이등변삼각형\n");
+    for (int i = 1; i <= temp; i++)
+    {
+        for (int j = temp; j > i ; j--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j < 2 * i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
