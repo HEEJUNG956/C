74b5d5dc1c6fcac217f4f9f6880b39a9914df421
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-24 11:39:58 +0900

    2

---
+++ C++ 열혈/프로그래밍 2-2 1번문제/프로그래밍 2-2 1번문제.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#pragma warning(disable:4996)
+using namespace std;
+int main()
+{
+    const int num = 12;
+    const int* ptr = &num;
+    const int* (&pointer) = ptr;
+    printf("포인터 변수의 값 : %d\n", *ptr);
+    printf("참조자의 값 : %d\n", *pointer);
+    return 0;
+}
---
+++ C++ 열혈/프로젝트 2-3 1번문제/프로젝트 2-3 1번문제.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#pragma warning (disable:4996)
+using namespace std;
+
+typedef struct __Point
+{
+    int xpos;
+    int ypos;
+}Point;
+
+Point& PntAdder(const Point& p1, const Point& p2)
+{
+    Point* result = new Point;
+    result->xpos = p1.xpos + p2.xpos;
+    result->ypos = p1.ypos + p2.ypos;
+    return *result;
+}
+
+int main()
+{
+    Point* ptr1 = new Point;
+    printf("ptr1의 xpos와 ypos의 입력 : ");
+    scanf("%d %d", ptr1->xpos, ptr1->ypos);
+
+    Point* ptr2 = new Point;
+    printf("ptr2의 xpos와 ypos의 입력 : ");
+    scanf("%d %d", ptr2->xpos, ptr2->ypos);
+
+    Point& result = PntAdder(*ptr1, *ptr2);
+    printf("결과의 xpos : %d\n", result.xpos);
+    printf("결과의 ypos : %d\n", result.ypos);
+
+    delete ptr1;
+    delete ptr2;
+    delete& result;
+    return 0;
+}
