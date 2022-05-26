81ed990e0f453eadb33a932f3f96b0a53ee91ea1
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-05-26 13:40:09 +0900

    Tab

---
+++ C++Project/Tab/CTab1.cpp
@@ -0,0 +1,34 @@
+﻿// CTab1.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "Tab.h"
+#include "afxdialogex.h"
+#include "CTab1.h"
+
+
+// CTab1 대화 상자
+
+IMPLEMENT_DYNAMIC(CTab1, CDialogEx)
+
+CTab1::CTab1(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1, pParent)
+{
+
+}
+
+CTab1::~CTab1()
+{
+}
+
+void CTab1::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CTab1, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CTab1 메시지 처리기
