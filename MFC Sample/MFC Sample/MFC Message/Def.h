f66d6322b985a9bf85146b67582281873a578104
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-02 11:46:02 +0900

    사용자 메시지 호출

---
+++ MFC Sample/MFC Sample/MFC Message/Def.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "afxwin.h"
+
+#define WM_USER_WN WM_USER + 1000 // 사용자 정의 메시지 선언
---
+++ MFC Sample/MFC Sample/MFC Message/MFC MessageDlg.h
@@ -0,0 +1,38 @@
+﻿
+// MFC MessageDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCMessageDlg 대화 상자
+class CMFCMessageDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCMessageDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_MESSAGE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+
+	// 사용자 메시지를 사용할 때 발생한 메시지 핸들러 생성
+	afx_msg LRESULT OnMyTestMessage(WPARAM wParam, LPARAM lPararm);
+
+	DECLARE_MESSAGE_MAP()
+};
