bf20f35332fb945fc386f5500ed5115a1fa4acae
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-02 10:49:29 +0900

    채팅프로그램(미완)

---
+++ MFC Sample/MFC Sample/채팅프로그램/SocCom.cpp
@@ -0,0 +1,22 @@
+﻿// SocCom.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "채팅프로그램.h"
+#include "SocCom.h"
+
+// CSocCom 멤버함수
+// 소켓 클래스와 메인윈도우를 연결시킴
+
+void CSocCom::Init(HWND hWnd)
+{
+	m_hWnd = hWnd;
+}
+
+
+void CSocCom::OnReceive(int nErrorCode)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	SendMessage(m_hWnd, UM_RECEIVE, 0, 0);
+	CSocket::OnReceive(nErrorCode);
+}
---
+++ MFC Sample/MFC Sample/채팅프로그램/채팅프로그램Dlg.h
@@ -0,0 +1,46 @@
+﻿
+// 채팅프로그램Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+#include "stdAfx.h"
+
+// C채팅프로그램Dlg 대화 상자
+class C채팅프로그램Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	C채팅프로그램Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	CSocServer m_socServer; // 서버용 소켓
+	CSocCom* m_socCom;		// 통신용 소켓
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MY_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+	
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	afx_msg LPARAM OnAccept(UINT wParam, LPARAM lParam);
+	afx_msg LPARAM OnReceive(UINT wParam, LPARAM lParam);
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strSend;
+	CListBox m_list;
+	CString m_strStatus;
+	afx_msg void OnClickedButtonSend();
+};
