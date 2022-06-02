e959e24119429acaf1f69b4d6efa2ceec1fa3f1d
HEEJUNG956 <96678897+HEEJUNG956@users.noreply.github.com>
2022-06-02 16:14:48 +0900

    TabControl

---
+++ MFC Sample/MFC Sample/MFC TimerThread/DlgTab5.cpp
@@ -0,0 +1,34 @@
+﻿// DlgTab5.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFC TimerThread.h"
+#include "afxdialogex.h"
+#include "DlgTab5.h"
+
+
+// CDlgTab5 대화 상자
+
+IMPLEMENT_DYNAMIC(CDlgTab5, CDialogEx)
+
+CDlgTab5::CDlgTab5(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG1_TAB5, pParent)
+{
+
+}
+
+CDlgTab5::~CDlgTab5()
+{
+}
+
+void CDlgTab5::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(CDlgTab5, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDlgTab5 메시지 처리기
